195c748bd6fdf1ebb3a1576382bf9b5d783d9c75
Arad Weinstein <arad.weinstein@post.runi.ac.il>
2025-01-21 16:47:20 +0200

    add remove method

--- ListIterator.java
+++ ListIterator.java
@@ -3,10 +3,13 @@ public class ListIterator {
 
     // current position in the list (cursor)
     public Node current;
+    // reference to the previous node, used for removal
+    private Node previous;
 
     /** Constructs a list iterator, starting at the given node */
     public ListIterator(Node node) {
         current = node;
+        previous = null;
     }
 
     /** Checks if this iterator has more nodes to process */
@@ -18,6 +21,19 @@ public class ListIterator {
     public MemoryBlock next() {
         Node currentNode = current;
         current = current.next;
+        previous = currentNode;
         return currentNode.block;
+    }
+
+     /** Removes the current node from the list */
+     public void remove(){
+        if (previous == null) {
+            throw new IllegalStateException("Cannot remove element before calling next()");            
+        }
+        
+        // Remove the current node by updating the previous node's next pointer
+        previous.next = current;  // Skip the current node, linking previous to current.next
+        current = previous.next;  // Move current pointer to the next node
+     
     }
 }
